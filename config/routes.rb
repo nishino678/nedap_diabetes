ff5c7c76ae4566e8f185eaa06b3c2fba446eb6c4
Wim F <nishino678@gmail.com>
2016-12-06 13:52:14 +0000

    Remember me

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get '/users',     to: 'users#index'
+  post '/users/1', to: 'sessions#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206091515) do
+ActiveRecord::Schema.define(version: 20161206131607) do
 
   create_table "answers", force: :cascade do |t|
     t.integer  "question_id"
@@ -75,6 +75,7 @@ ActiveRecord::Schema.define(version: 20161206091515) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
