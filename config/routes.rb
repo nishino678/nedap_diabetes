2f4fbaca45005976406258fec88c7a5af74f94a7
Wim F <nishino678@gmail.com>
2016-12-06 13:08:31 +0000

    Basic login

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   get 'users/new'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get '/users',     to: 'users#index'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
