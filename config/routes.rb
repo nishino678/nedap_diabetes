a43bef9d9dcc9e4e137309f7d113f31e0cae29f2
Moederbeer <pauldgroot@gmail.com>
2016-12-08 18:03:11 +0100

    Fixing the damned route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get '/users',     to: 'users#index'
+  post '/users/:id', to: 'sessions#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
