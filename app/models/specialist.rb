63fc322ff75def9a8d359e9683a8fab47a30f4db
Moederbeer <pauldgroot@gmail.com>
2016-12-12 18:04:48 +0100

    Adding error messages/basic validation to article

--- app/models/specialist.rb
+++ app/models/specialist.rb
@@ -1,4 +1,6 @@
 class Specialist < ApplicationRecord
+  validates :name, presence: true
+  validates :bio, presence: true
   has_many :articles, inverse_of: :specialist
   accepts_nested_attributes_for :articles
 end
