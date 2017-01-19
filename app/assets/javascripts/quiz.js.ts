3158c59750dd14ad93033eb5777ac8931602dd71
Erin Blauw <erin@blue-arachnid.com>
2017-01-19 23:21:47 +0100

    initial quiz commit

--- app/assets/javascripts/quiz.js.ts
+++ app/assets/javascripts/quiz.js.ts
@@ -1,3 +1,4 @@
+// Flash messages JS
 var elem = document.getElementsByClassName("alert");
 
 function listen(elem:any) {
@@ -8,6 +9,49 @@ function listen(elem:any) {
   }
 }
 
+///////////////////////// QUIZIES
+
+function answerListener(){
+  var answers = document.getElementsByClassName('answer');
+  for (var i = 0; i < answers.length; ++i) {
+    var answer = answers[i];
+    answer.addEventListener('click', function(){
+      checkCorrectness(this);
+      // this.classList.add("chosen");
+    })
+  }
+}
+
+function checkCorrectness(element:any) {
+  var test = document.getElementsByClassName("answer"),
+      correctness = element.getAttribute("data-correct");
+  addClass(element, correctness);
+}
+
+function addClass(element:Element, correctness:string|boolean) {
+  var alreadyHasClass = element.classList.contains("wrong")||element.classList.contains("right");
+  if (!alreadyHasClass){
+    if(correctness == "true"){
+      element.classList.add("correct");
+    } else {
+      element.classList.add("wrong");
+    }
+  }
+}
+
+////////////
+
+function quizListener(){
+  var answersClusters = document.getElementsByClassName('answers');
+  for (var i = 0; i < answersClusters.length; ++i) {
+    var children = answersClusters[i];
+    // console.log(child[0]);
+    // getAnswers(children);
+  }
+}
+
 window.onload = function() {
   listen(elem);
-};
+  // quizListener();
+  answerListener();
+ };
