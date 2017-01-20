beb1f97688e531a82fcf40d24bcfcb269f7d1886
Paul de Groot <pauldgroot@gmail.com>
2017-01-20 19:17:47 +0000

    Added seed, admin login to admin@example.com, splitted seed for convenience.

--- db/seeds/4_create_articles.rb
+++ db/seeds/4_create_articles.rb
@@ -101,11 +101,11 @@ Article.create!(
   article_image: File.open(File.join(Rails.root, 'db', 'seed_images', 'nederland.jpg')),
   title: "Naar verhouding weinig diabetespatiënten in Nederland",
   author: "ANP",
-  content: 
-  "<p><b>In Nederland zijn vergeleken met andere Europese landen relatief weinig mensen met diabetes, oftewel suikerziekte. Dat maakt het Centraal Bureau voor de Statistiek (CBS) bekend.</p</b>
+  content: "
+  <p><b>In Nederland zijn vergeleken met andere Europese landen relatief weinig mensen met diabetes, oftewel suikerziekte. Dat maakt het Centraal Bureau voor de Statistiek (CBS) bekend.</p></b>
   <p>Alleen in Roemenië, Zweden, België, Oostenrijk, Denemarken, Letland en Litouwen is het aandeel inwoners van vijftien jaar of ouder met diabetes lager dan in Nederland. In Frankrijk is dit aandeel juist het hoogst, meldt het CBS vrijdag.</p>
   <p>Het aantal diabetespatiënten in Nederland is wel gestegen. In totaal heeft bijna 4,8 procent van de Nederlanders diabetes, tegen 2,8 procent in 2001. Vooral het aantal patiënten met het veelvoorkomende diabetes type 2 is gestegen.</p>
-  <h1>Obesitas</h>
+  <h1>Obesitas</h1>
   <p>Vorig jaar had bijna 12 procent van de Nederlanders obesitas (ernstig overgewicht). Ruim 17 procent van hen rapporteerde tevens diabetes, tegenover ruim 3 procent van de mensen zónder obesitas. Het CBS vroeg mensen of ze suikerziekte hebben en vroeg ook naar hun lengte en gewicht.</p>
   <p>Obesitas is, net als ongezonde voeding, lichamelijke passiviteit en roken een risicofactor voor het krijgen van diabetes type 2.</p>
   ",
@@ -113,6 +113,56 @@ Article.create!(
   specialist_id: "3",
   all_tags: "diabetes, nederland, obesitas, type2, cbs",
   explanation: "Nederland is een ontwikkeld land, hierdoor letten mensen meer op hun voeding en maken ze bewustere keuzes. Type 2 diabetes is wel een ernstig probleem en Nederlanders moeten zich bewuster worden van hun bewegings- en eetpatroon.",
+  questions_attributes: [
+  {
+     content: 'Vraag 1',
+     answers_attributes: [
+       {content: "Antwoord 1 op vraag 1",
+        correct: true},
+       {content: "Antwoord 2 op vraag 1"},
+       {content: "Antwoord 3 op vraag 1"}
+     ]
+   },
+
+   {
+     content: 'Vraag 2',
+     answers_attributes: [
+       {content: "Antwoord 1 op vraag 2"},
+       {content: "Antwoord 2 op vraag 2",
+        correct: true},
+       {content: "Antwoord 3 op vraag 2"}
+     ]
+   },
+
+   {
+     content: 'Vraag 3',
+     answers_attributes: [
+       {content: "Antwoord 1 op vraag 3"},
+       {content: "Antwoord 2 op vraag 3"},
+       {content: "Antwoord 3 op vraag 3",
+        correct: true}
+     ]
+   }
+ ]
+)
+
+Article.create!(
+  article_image: File.open(File.join(Rails.root, 'db', 'seed_images', 'bedrust.jpg')),
+  title: "Bedrust kan leiden tot verhoogde kans op diabetes",
+  author: "NU.nl",
+  content: "
+  <p><b>Een week bedrust kan mogelijk al leiden tot een verhoogd risico op het ontwikkelen van diabetes type 2. Bovendien levert het ook een gemiddeld spiermassaverlies op van anderhalve kilo.</b></p>
+  <p>Dat concluderen wetenschappers van het Maastricht UMC+ naar aanleiding van een onderzoek waaraan gezonde vrijwilligers hebben meegewerkt.  De resultaten van het onderzoek zijn recent gepubliceerd in het wetenschappelijk tijdschrift <em>Diabetes</em>.</p>
+  <p>De onderzoekers lieten tien gezonde jongemannen een week gekluisterd aan hun bed doorbrengen, wat vergelijkbaar is met een gemiddelde ziekenhuisopname. Tijdens deze periode maten de onderzoekers op verschillende momenten de insulinegevoeligheid, spierkracht, spiermassa en bloedsamenstelling van de mannen.</p>
+  <p>Hierbij werd de voeding die de vrijwilligers gedurende de week kregen vooraf vastgesteld en nauwlettend gecontroleerd. Na een week waren de proefpersonen 1,4 kilo spiermassa verloren, wat zo’n 2,5 procent van het totaal is.</p>
+  <h1>Hogere bloedsuikerspiegel</h1>
+  <p>Bovendien was de insulinegevoeligheid afgenomen met zo’n 30 procent. Dit heeft als gevolg dat het lichaam minder goed in staat is om suikers om te zetten, wat leidt tot een hogere bloedsuikerspiegel en wat uiteindelijk een risico vormt tot het ontwikkelen van diabetes.</p>
+  <p><em>'Ondanks de kleine onderzoekspopulatie zijn de resultaten duidelijk waarneembaar'</em>, zegt hoofdonderzoeker Marlou Dirks. <em>'Door de effecten van bedrust verder in kaart te brengen, kunnen we ook werken aan het voorkomen of het herstellen van de negatieve gevolgen daarvan. Zo is het bijvoorbeeld mogelijk om met voeding en activiteit op de juiste momenten van de dag te zorgen voor een minimaal spiermassaverlies. Nu moeten we dus ook rekening houden met de insulinegevoeligheid.'</em></p>
+  ",
+  source: "http://www.nu.nl/gezondheid/4296005/bedrust-kan-leiden-verhoogde-kans-diabetes.html",
+  specialist_id: "2",
+  all_tags: "diabetes, bedrust, bloedsuikerspiegel, obesitas, type2",
+  explanation: "Als je bedrust nodig hebt door een aandoening of letsel zijn er een hoop andere manieren om toch je beweging te krijgen, vraag je verpleegkundige voor meer informatie.",
   questions_attributes: [
   {
      content: 'Vraag 1',
