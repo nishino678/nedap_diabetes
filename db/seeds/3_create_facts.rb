816f96f8dd0b734e6a49f8a14b5fcce810d1efe2
Paul de Groot <pauldgroot@gmail.com>
2017-01-29 22:36:27 +0000

    Some final touches.

--- db/seeds/3_create_facts.rb
+++ db/seeds/3_create_facts.rb
@@ -1,59 +1,259 @@
 Fact.create!(
-  title:  "Bѐtablokkers",
-  content: "Medicijnen diegebruikt worden bij o.a. hoge bloeddruk en angina pectoris. Zij kunnen het herstelvan een hypo vertragen en ervoor zorgen dat men een optredende hypo niet voeltaankomen.",
+  title:  "A1c",
+  content: "Een test die laat zien hoe goed uw bloedsuiker (glucose) is gecontroleerd in de afgelopen drie maanden.",
  )
 
 Fact.create!(
-  title:  "Glucose",
-  content: "Druivensuiker. Een enkelvoudige suiker. Deze circuleert in het bloed en heet ook wel bloedsuiker.",
+  title:  "Alvleesklier",
+  content: "Albumine is een bepaald eiwit in het bloed. Als in de urine meer dan een normale hoeveelheid albumine zit, heet dat albuminurie. Dat is dan een aanwijzing voor nierschade (nefropathie).",
  )
 
 Fact.create!(
-  title:  "BMI",
-  content: "Maat om het overgewicht teberekenen m.b.v. de formule kg/m2",
+  title:  "Atherosclerose / Aderverkalking",
+  content: "Bij Atherosclerose, ook wel aderverkalking genoemd, hebben zich aan de wand van de bloedvaten  deeltjes vetten (triglyceriden, LDL-cholesterol) afgezet. Daardoor zijn de bloedvaten vernauwd.",
+ )
+
+Fact.create!(
+  title:  "Bètacellen",
+  content: "Bètacellen zijn cellen in de alvleesklier die insuline aanmaken. Ze zitten in de zogeheten eilandjes van Langerhans.",
+ )
+
+Fact.create!(
+  title:  "Bloedglucosegehalte",
+  content: "De hoeveelheid suikers, glucose, in het bloed. Wordt ook wel suikergehalte, glucosegehalte of bloedsuikerspiegel genoemd. Het bloedglucosegehalte wordt aangeduid in mmol/liter, en heet dan een bloedglucosewaarde. Als je diabetes hebt en insuline gebruikt, dan moet je meerdere malen per dag zelf je bloedglucosewaarde bepalen met een vingerprik.",
+ )
+
+Fact.create!(
+  title:  "Body Mass Index (BMI)",
+  content: "Body Mass Index (BMI) is een maat voor de verhouding tussen je gewicht en lengte.",
+ )
+
+Fact.create!(
+  title:  "Cholesterol",
+  content: "Cholesterol is een belangrijke stof voor het lichaam. Het is een bouwstof voor lichaamscellen. Lees alles over cholesterol en gezondheidsrisico’s op diabetesfonds.nl. Cholesterol is een belangrijke stof voor het lichaam. Het is een bouwstof voor lichaamscellen.",
+ )
+
+Fact.create!(
+  title:  "Complicaties",
+  content: "Bijverschijnselen of gevolgen van een ziekte, in dit geval diabetes. Als mensen met diabetes erg vaak te veel suikers in hun bloed hebben, kan er veel mis gaan in het lichaam. Bloedvaten en zenuwen lopen schade op.  Er kunnen problemen komen met ogen, nieren, hart en bloedvaten en zenuwen.",
+ )
+
+Fact.create!(
+  title:  "Diabetescomplicaties",
+  content: "Bijverschijnselen of gevolgen van diabetes. Als mensen met diabetes erg vaak te veel suikers in hun bloed hebben, kan er veel mis gaan in het lichaam. Bloedvaten en zenuwen lopen schade op.  Er kunnen problemen komen met ogen, nieren, hart en bloedvaten en zenuwen.",
+ )
+
+Fact.create!(
+  title:  "Diabetische voet",
+  content: "Diabetische voet is een andere benaming voor voetproblemen bij mensen met diabetes. Zenuwen en bloedvaatjes raken beschadigd wanneer de bloedsuiker te vaak te hoog is. Dit leidt tot minder gevoel in de voeten, waardoor wondjes niet worden opgemerkt en ook minder goed genezen. Een simpel wondje kan uitgroeien tot een ontsteking of zweer (ulcus), die vaak slecht geneest. In het ergste geval moet een teen, een voet of zelfs delen van het been geamputeerd worden.",
  )
 
 Fact.create!(
   title:  "Eilandjes van Langerhans",
-  content: "Eengroep cellen in de alvleesklier. Bevat o.a. de bѐtacellen in de alvleesklier enook cellen die glucagon maken.",
+  content: "Insuline wordt gemaakt in de alvleesklier. De cellen die insuline maken, zitten bij elkaar in groepjes. Door de ronde vorm lijken deze groepjes cellen onder de microscoop op een eilandje. Daarom worden deze  groepjes ‘eilandjes’ van Langerhans genoemd. Ze zijn genoemd naar  de Duitse wetenschapper Paul Langerhans, die ze in 1869 ontdekte.",
  )
 
 Fact.create!(
-  title:  "Bѐtacellen",
-  content: "Dit zijn cellen dieinsuline maken. Ze bevinden zich in de Eilandjes van Langerhans in de alvleesklier.",
+  title:  "Eilandjestransplantatie",
+  content: "Al in 1985 vond voor het eerst een transplantatie van de eilandjes van Langerhans plaats. Sinds een paar jaar gebeurt dit ook in Nederland, maar wereldwijd nog steeds  maar weinig. De reden hiervoor is dat er veel nadelen zijn. Er is  wel veel onderzoek naar hoe de techniek verbeterd kan worden. Zo ook in Nederland, mogelijk gemaakt door het Diabetes Fonds.",
  )
 
 Fact.create!(
-  title:  "Hypoglycemie",
-  content: "Een verlaagde bloedglucosewaarde.",
- )
-
-Fact.create!(
-  title:  "Hyperglycemie",
-  content: "Een verhoogde bloedglucosewaarde.",
- )
-
-Fact.create!(
-  title:  "Vasculopathie",
-  content: "Afwijkingen aan de grote en kleine bloedvaten. ",
- )
-
-Fact.create!(
-  title:  "Nefropathie",
-  content: "Afwijkingen aan denieren.",
- )
-
-Fact.create!(
-  title:  "Pancreas",
-  content: "Alvleesklier, dit is een orgaan in de buikholte achter de maag waar hormonen als insuline englucagon worden gemaakt.",
+  title:  "Gastroparese",
+  content: "Gastroparese heet ook wel 'vertraagde maaglediging', de maag houdt het eten te lang vast. Na verloop van tijd kan het hele zenuwstelsel lijden onder diabetes, ook de zenuwen die de maagbewegingen regelen. Wanneer dat regelsysteem niet meer goed werkt, wordt de snelheid waarmee voedsel vanuit de maag in de darmen komt onvoorspelbaar. Daardoor krijgen mensen soms last van een opgeblazen gevoel, misselijkheid en buikpijn. Naar schatting hebben drie tot vijf van de tien mensen met diabetes hiermee te maken. Soms zijn de klachten niet zo opvallend of weten mensen niet dat het door diabetes komt.",
  )
 
 Fact.create!(
   title:  "Glucagon",
-  content: "Hormoon dat wordtgemaakt in de alvleesklier en een bloedglucosegehalte verhogende werking heeft. Het is ook beschikbaar als medicijn dat kan worden ingespoten bij een lagebloedglucosewaardes.",
+  content: "Glucagon is een hormoon dat aangemaakt wordt in de alvleesklier, net als insuline. Wel wordt Glucagon door andere cellen geproduceerd. Glucagon wordt gemaakt door ‘alfacellen’ en insuline door ‘bètacellen’. Beide soorten cellen zitten bij elkaar in de eilandjes van Langerhans. Insuline en glucagon regelen samen de bloedsuikerspiegel.",
+ )
+
+Fact.create!(
+  title:  "Glucose",
+  content: "Glucose is een vorm van suiker, ook wel druivensuiker genoemd, die onze lichaamscellen omzetten in energie.",
+ )
+
+Fact.create!(
+  title:  "Glucosetolerantie",
+  content: "De mate waarin het lichaam glucose (suiker) uit het bloed kan verwerken heet ‘glucosetolerantie’.",
+ )
+
+Fact.create!(
+  title:  "Glykemische index",
+  content: "Voor alle voedingsmiddelen kan worden berekend hoe snel ze de bloedsuikerspiegel doen stijgen. Die berekening leidt tot de glykemische index.",
+ )
+
+Fact.create!(
+  title:  "HbA1c",
+  content: "Minstens een keer per jaar krijgen mensen met diabetes een controle voor hun gemiddelde bloedsuiker. Dat heet HbA1c. De HbA1c-waarde zegt iets over de gemiddelde bloedsuiker in de afgelopen 2 tot 3 maanden. Hij geeft de hoeveelheid versuikerd hemoglobine in het bloed aan, een stof waardoor rode bloedcellen hun kleur krijgen. De test meet hoeveel bloedsuiker zich heeft gehecht aan die hemoglobine. Het HbA1c werd tot april 2010 uitgedrukt in een getal met procenten, maar nu in mmol/mol. Bekijk het verschil tussen oude en nieuwe waarden in een omrekentabel. Een goede regulatie zit onder de 53 mmol/mol (7%). Let op: het oude getal 7% is wat anders dan een bloedsuiker van 7 mmol/l! ",
+ )
+
+Fact.create!(
+  title:  "Honeymoonperiode / honeymoonfase",
+  content: "Wanneer iemand de diagnose diabetes type 1 krijgt, moet hij of zij meteen beginnen met het regelmatig spuiten van insuline.  Vaak volgt al snel een periode waarin minder insuline nodig is. Het is dan eenvoudiger om de bloedsuiker te regelen en stabiel te houden. Dit heet  de honeymoonfase (wittebroodsweken) genoemd. Het lijkt dan even bijna alsof het wel meevalt met de diabetes.",
+ )
+
+Fact.create!(
+  title:  "Hoornstudie",
+  content: "De Nederlandse ‘Hoornstudie’ zette rond het jaar 2000 de wereld op zijn kop. Want daaruit bleek dat er veel meer mensen met diabetes type 2 waren dan gedacht, zo’n acht procent van de bevolking. De helft van de mensen liep er al jaren ongemerkt mee rond zonder het zelf te weten. En deze mensen bleken al schade aan hart, bloedvaten en ogen te hebben. Het onderzoek heet trouwens zo omdat het is gedaan bij inwoners van de stad Hoorn. Naar aanleiding van dit onderzoek, betaald door het Diabetes Fonds, is iedereen internationaal meer gaan opletten om diabetes eerder te herkennen en te behandelen. Dankzij de Hoornstudie is de diabeteszorg in Nederland nu een stuk beter georganiseerd. Inmiddels is de kans op overlijden bij diabetes type 2 gehalveerd.",
+ )
+
+Fact.create!(
+  title:  "Hyper",
+  content: "Afkorting van hyperglykemie, te hoge bloedsuiker.",
+ )
+
+Fact.create!(
+  title:  "Hyperglykemie",
+  content: "Wordt ook 'een hyper' genoemd. Situatie waarbij het bloedglucosegehalte te hoog is, namelijk boven de 8-10 mmol/liter.",
+ )
+
+Fact.create!(
+  title:  "Hypertensie",
+  content: "Hypertensie is het medische woord voor hoge bloeddruk. Veel mensen met diabetes hebben hier last vast. Er is nog discussie over hoe hoog (of laag) de bloeddruk bij diabetes zou moeten zijn. De grens is in de afgelopen jaren steeds verder naar beneden bijgesteld naar aanleiding van onderzoeksresultaten. Op dit moment wordt een bloeddruk onder de 130/85 mmHg als ideaal gezien. Hoe hoog of laag de bloeddruk precies moet zijn, bepaalt de dokter per persoon. Bij een hoge bloeddruk is over het algemeen sprake van een bovendruk van meer dan 160 mmHG en een onderdruk van meer dan 95 mmHG.",
+ )
+
+Fact.create!(
+  title:  "Hypo",
+  content: "Afkorting van hypoglykemie, te lage bloedsuiker.",
+ )
+
+Fact.create!(
+  title:  "Hypoglykemie",
+  content: "Wordt ook 'een hypo' genoemd. Situatie waarbij de bloedsuikerspiegel te laag is, namelijk onder de 4 mmol/liter.",
+ )
+
+Fact.create!(
+  title:  "Hypo-unawareness",
+  content: "Engels woord voor het niet voelen aankomen van een hypo, aanval van te lage bloedsuiker. Kan na verloop van tijd gebeuren vooral bij mensen met diabetes type 1. Soms ook bij type 2. Het lichaam geeft geen duidelijke signalen meer waaraan je een hypo merkt. Er zijn trainingen om hypo-unawareness te verminderen. ",
+ )
+
+Fact.create!(
+  title:  "Instellen",
+  content: "Onderzoek van een arts naar hoeveel insuline iemand met diabetes nodig heeft om de bloedsuikerspiegel stabiel te houden. Ieder mens is wat dat betreft uniek: iedere persoon met diabetes heeft een eigen instelling. Bij sommige mensen is het heel moeilijk om een stabiele instelling te bereiken, bij anderen gaat het zonder veel problemen.",
  )
 
 Fact.create!(
   title:  "Insuline",
-  content: "Dit is een hormoon datin de alvleesklier wordt gemaakt. Het zorgt dat glucsoe uit het bloed de cellen in de weefsels in kan en zo verlaagd het, het bloedglucosegehalte.",
+  content: "Natuurlijk hormoon dat gemaakt wordt in de alvleesklier. Insuline zorgt ervoor dat glucose uit het bloed kan worden opgenomen in de lichaamscellen. Glucose geeft de cellen de energie die ze nodig hebben. Veel mensen met diabetes injecteren insuline bij zichzelf. Daarvoor zijn er verschillende soorten insuline, variërend van snel- en kortwerkende insulines tot langwerkende insulines.",
+ )
+
+Fact.create!(
+  title:  "Insulinepen",
+  content: "Een insulinepen is een soort pen die lijkt op een vulpen, met een heel dun injectienaaldje. In de pen zit een vulling met insuline. Je kunt instellen hoeveel insuline je per keer spuit.",
+ )
+
+Fact.create!(
+  title:  "Insulinepomp",
+  content: "Het lichaam is minder gevoelig of ongevoelig voor insuline. Daardoor wordt bloedsuiker niet meer goed uit het bloed gehaald. Er komt veel kijken bij insulineresistentie, en er wordt volop onderzoek gedaan naar wat precies de achterliggende redenen en mechanismen zijn. Overgewicht speelt in ieder geval een grote rol.",
+ )
+
+Fact.create!(
+  title:  "Jeugddiabetes",
+  content: "Jeugddiabetes is een verouderd woord voor diabetes type 1. Vroeger werd diabetes type 1 zo genoemd omdat de meeste mensen onder de 30 jaar zijn als ze het krijgen. Het is wat misleidend om het jeugddiabetes te noemen, want diabetes type 1 houd je de rest van je leven. Ook kun je het op alle leeftijden nog krijgen.",
+ )
+
+Fact.create!(
+  title:  "Keto-acidose",
+  content: "Bij een tekort aan insuline kan de bloedsuiker heel hoog oplopen. Het lichaam zoekt een andere brandstof en gaat vetten verbranden. Daarbij komen afbraakstoffen vrij: ketonen. Die verzuren letterlijk het bloed: keto-acidose. Symptomen zijn een snelle diepe ademhaling en braken. De adem ruikt naar aceton. Keto-acidose moet snel door een arts worden behandeld, anders kan iemand in coma raken. Keto-acidose komt voornamelijk bij diabetes type 1 voor.",
+ )
+
+Fact.create!(
+  title:  "Koolhydraten",
+  content: "Verzamelnaam voor suikers en zetmeel. Koolhydraten zitten bijvoorbeeld in fruit, suiker, melk, brood, aardappelen, macaroni en rijst. ",
+ )
+
+Fact.create!(
+  title:  "LADA",
+  content: "Afkorting van de Engelse naam Latent Autoimmune Diabetes in Adults. Mensen met LADA krijgen in de praktijk meestal de diagnose diabetes type 2 omdat ze vaak nog een tijdje zonder insuline spuiten toe kunnen. Vooral naar verhouding jonge, slanke mensen die ‘type 2’ diabetes krijgen, blijken vaak LADA te hebben. Zij kunnen beter direct worden behandeld met insuline in plaats van tabletten. Naar schatting heeft 15 procent van de mensen met de diagnose type 2 diabetes eigenlijk de LADA-vorm.",
+ )
+
+Fact.create!(
+  title:  "Metabool syndroom",
+  content: "Veel mensen met type 2 diabetes hebben, al voordat ze diabetes krijgen, last van verschillende problemen in het lichaam: het metabool syndroom.",
+ )
+
+Fact.create!(
+  title:  "Mmol/liter",
+  content: "De bloedsuikerwaarde wordt gemeten in mmol per liter) en geeft het aantal millimol glucose per liter bloed aan.",
+ )
+
+Fact.create!(
+  title:  "MIDD",
+  content: "Afkorting van het Engelse Maternally Inherited Diabetes and Deafness. Een zeldzame vorm van diabetes die erfelijk via de moeder wordt overgedragen. Patiënten hebben vaak last van gehoorverlies. MIDD lijkt zowel op diabetes type 1 als op type 2. Het kan vaak eerst een tijd behandeld worden met alleen tabletten.",
+ )
+
+Fact.create!(
+  title:  "MODY",
+  content: "Afkorting van de Engelse naam Maturity Onset Diabetes of the Young. Vorm van diabetes die op jonge leeftijd begint en waarvan de aanleg erfelijk is. Deze vorm wordt vaak ingeschat als diabetes type 1, en wordt daardoor behandeld met insuline. Iemand met MODY kan echter vaak nog een tijd toe met tabletten. Zo’n 5 procent van de mensen met diabetes heeft MODY.",
+ )
+
+Fact.create!(
+  title:  "Nefropathie",
+  content: "Nieraandoening die kan ontstaan bij mensen met diabetes. De nieren zuiveren het bloed van afvalstoffen. Door diabetes kan er te veel bindweefsel in de nieren ontstaan, waardoor ze steeds minder goed kunnen filteren. Hierdoor kunnen afvalstoffen toch in het lichaam blijven die anders via de urine het lichaam zouden verlaten.",
+ )
+
+Fact.create!(
+  title:  "Neuropathie",
+  content: "Neuropathie is het medische woord voor zenuwschade. Zenuwen in het hele lichaam kunnen schade oplopen als de bloedsuiker regelmatig te hoog is.",
+ )
+
+Fact.create!(
+  title:  "NIDDM",
+  content: "Engelse afkorting voor Non-Insulin-Dependent Diabetes Mellitus, oftewel niet-insuline-afhankelijke diabetes. Dat is een medische term voor diabetes type 2. Al gebruiken veel mensen met diabetes type 2 ook insuline, ze zijn er meestal niet helemaal van afhankelijk om te overleven, zoals mensen met diabetes type 1.",
+ )
+
+Fact.create!(
+  title:  "Orale glucose tolerantie test (OGTT)",
+  content: "Ook suikerwatertest genoemd. Artsen bepalen soms met deze test of iemand problemen met de suikerstofwisseling (glucosehuishouding), en dus (bijna) diabetes, heeft. Na het drinken van suikerwater wordt gemeten hoe snel dat door het lichaam wordt verwerkt.",
+ )
+
+Fact.create!(
+  title:  "Ouderdomsdiabetes",
+  content: "Verouderd woord voor diabetes type 2. Vroeger werd het zo genoemd omdat vooral oudere mensen diabetes type 2 kregen. Tegenwoordig kunnen mensen van alle leeftijden het krijgen. Daarom is het woord ouderdomsdiabetes (of ouderdomssuiker) afgeschaft.",
+ )
+
+Fact.create!(
+  title:  "Pancreas",
+  content: "Alvleesklier. Een orgaan in de buurt van de maag dat onder andere insuline aanmaakt.",
+ )
+
+Fact.create!(
+  title:  "Prediabetes",
+  content: "Prediabetes  is een voorfase van diabetes type 2. Het lichaam wordt in deze fase al minder gevoelig voor insuline en heeft hierdoor meer moeite met de verwerking van glucose (bloedsuiker). De bloedsuikerwaarden zijn al iets hoger, maar nog niet zo hoog dat iemand diabetes heeft. Mensen met prediabetes hebben ook nog niet de symptomen van diabetes, of ze hebben die slechts een beetje.",
+ )
+
+Fact.create!(
+  title:  "Retinopathie",
+  content: "Oogaandoening. Diabetes beschadigt de kleine bloedvaatjes in het netvlies van de ogen. Hierdoor ontstaan bloedinkjes en littekenweefsel. Nieuw aangelegde bloedvaatjes zijn zo teer dat ze ook vaak kapot gaan, wat ook weer zorgt voor bloedinkjes. Dit leidt op den duur tot slechtziendheid. Zonder behandeling kan het zelfs uitmonden in blindheid.",
+ )
+
+Fact.create!(
+  title:  "Suikerziekte",
+  content: "Suikerziekte is een ander woord voor diabetes mellitus. Het woord suikerziekte wekt de indruk dat diabetes komt door te veel suiker eten of dat mensen met diabetes geen suiker mogen eten. Dit klopt niet.",
+ )
+
+Fact.create!(
+  title:  "Stamcellen",
+  content: "Stamcellen zijn cellen die zich kunnen blijven delen. Stamcellen komen overal voor in ons lichaam.",
+ )
+
+Fact.create!(
+  title:  "Triglyceriden",
+  content: "Deeltjes vet in het bloed die niet oplossen maar aan de wanden van de bloedvaten gaan plakken. Dat kan leiden tot aderverkalking. De hoeveelheid triglyceriden hangt samen met voeding, lichaamsbeweging, roken, erfelijke aanleg en eventuele medicijnen.",
+ )
+
+Fact.create!(
+  title:  "Ulcus / ulceratie",
+  content: "Zweer aan voeten die kan ontstaan als bijvoorbeeld een ontstoken wondje uit de hand loopt. Door diabetes wordt de doorbloeding in de benen en voeten slechter. Zenuwen raken beschadigd en er zit minder gevoel in de voeten. Daardoor worden wondjes minder snel opgemerkt, en infecties genezen ook moeilijker. Door die zenuwuitval kunnen mensen ook een andere houding met lopen aannemen. De drukveranderingen in de voet die daardoor ontstaan, kunnen op hun beurt ook leiden tot zweren.",
+ )
+
+Fact.create!(
+  title:  "Zelfcontrole",
+  content: "Heb je diabetes, dan moet je een paar keer per dag zelf je bloedglucosespiegel (bloedsuiker) controleren. Dat doe je met een glucosemeter, die in een druppel bloed het glucosegehalte meet. Daarvoor moet je in je vinger prikken met een speciaal vingerprik-apparaatje.",
+ )
+
+Fact.create!(
+  title:  "Zwangerschapsdiabetes",
+  content: "Zwangerschapsdiabetes is een tijdelijke vorm van diabetes die kan ontstaan na de 24e week van de zwangerschap. Dat gebeurt onder invloed van de hormonen die worden aangemaakt tijdens de zwangerschap. Die hormonen remmen de werking van insuline af. Daardoor wordt de bloedsuikerspiegel te hoog. Na de bevalling verdwijnt de diabetes heel snel, meestal binnen 24 uur.",
  )
